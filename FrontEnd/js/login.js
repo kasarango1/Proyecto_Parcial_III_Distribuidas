33f7c41b83fbb806cb9462d794e9205a903ea956
Katherine Sarango <107556558+kasarango1@users.noreply.github.com>
2025-03-07 11:13:33 -0500

    Delete FrontEnd directory

--- FrontEnd/js/login.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("loginForm").addEventListener("submit", async (event) => {
-    event.preventDefault();
-    const correo = document.getElementById("correo").value;
-    const contraseña = document.getElementById("contraseña").value;
-    await login(correo, contraseña);
-});
